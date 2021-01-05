20690a24ce606a67ee6971609c91ca8704aa7b66
TomZ <tom@flowee.org>
2021-01-05 14:03:30 +0100

    Update email

--- Payment.h
+++ Payment.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Flowee project
- * Copyright (C) 2020 Tom Zander <tomz@freedommail.ch>
+ * Copyright (C) 2020 Tom Zander <tom@flowee.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
