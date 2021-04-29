d2b0d1ae1fc3751bf56032bd12cf43b52454fcd1
TomZ <tom@flowee.org>
2021-04-29 16:34:35 +0200

    Re-enable network

--- main.cpp
+++ main.cpp
@@ -130,7 +130,7 @@ int main(int argc, char *argv[])
             app->p2pNet()->connectionManager().peerAddressDb().addOne( // actually connect to it too.
                         EndPoint(parser.value(connect).toStdString(), 8333));
         }
-        // app->p2pNet()->start(); // lets go!
+        app->p2pNet()->start(); // lets go!
     });
 
     // Clean shutdown on SIGTERM
