eb3c3f63dbc588b9ebc5a714db981d700d571b96
TomZ <tom@flowee.org>
2021-11-19 14:20:52 +0100

    Increase version number

--- main.cpp
+++ main.cpp
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
     QGuiApplication qapp(argc, argv);
     qapp.setOrganizationName("flowee");
     qapp.setApplicationName("pay");
-    qapp.setApplicationVersion("2021.06");
+    qapp.setApplicationVersion("2021.06.1");
     qapp.setWindowIcon(QIcon(":/FloweePay.png"));
 
     srand((quint32) QDateTime::currentMSecsSinceEpoch());
