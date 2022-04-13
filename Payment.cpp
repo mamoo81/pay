b7b191211df93d1c9cd639284fdc364ad7f85c6c
TomZ <tom@flowee.org>
2022-04-13 10:51:15 +0200

    Fix removing of detail not changing validness
    In case an incorrect payment got a detail removed, leaving us with a
    fully correct payment we didn't validate it and 'prepare' would not be
    made available.

--- Payment.cpp
+++ Payment.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Flowee project
- * Copyright (C) 2020-2021 Tom Zander <tom@flowee.org>
+ * Copyright (C) 2020-2022 Tom Zander <tom@flowee.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -441,6 +441,7 @@ void Payment::remove(PaymentDetail *detail)
     const auto count = m_paymentDetails.removeAll(detail);
     if (count) {
         emit paymentDetailsChanged();
+        emit validChanged();
 
         // Make sure only the last output has 'maxAllowed' set to true.
         if (detail->isOutput()) {
