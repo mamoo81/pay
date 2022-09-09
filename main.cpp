f177b57ca7fb692340f1a79257d3f6d9779a2e30
TomZ <tom@flowee.org>
2022-09-10 00:51:00 +0200

    new version

--- main.cpp
+++ main.cpp
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
     QGuiApplication qapp(argc, argv);
     qapp.setOrganizationName("flowee");
     qapp.setApplicationName("pay");
-    qapp.setApplicationVersion("2022.08.0");
+    qapp.setApplicationVersion("2022.09.0");
     qapp.setWindowIcon(QIcon(":/FloweePay.png"));
 
     srand((quint32) QDateTime::currentMSecsSinceEpoch());
