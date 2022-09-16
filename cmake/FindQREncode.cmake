0be7088b9725c5222e657c4972c3a1818c0ed35a
TomZ <tom@flowee.org>
2022-09-16 20:49:15 +0200

    Add QR-encode lib to android build

--- cmake/FindQREncode.cmake
+++ cmake/FindQREncode.cmake
@@ -24,24 +24,29 @@ find_brew_prefix(BREW_HINT qrencode)
 find_package(PkgConfig)
 pkg_check_modules(PC_QREncode QUIET libqrencode)
 
-find_path(QREncode_INCLUDE_DIR
-	NAMES qrencode.h
-	HINTS ${BREW_HINT}
-	PATHS ${PC_QREncode_INCLUDE_DIRS}
-)
+if (ANDROID)
+    if (EXISTS "/opt/android-qrencode/include/qrencode.h")
+        set (QREncode_INCLUDE_DIR "/opt/android-qrencode/include")
+        set (QREncode_FOUND TRUE)
+        set (QREncode_LIBRARIES "/opt/android-qrencode/lib/libqrencode.a")
+    endif()
+else()
+    find_path(QREncode_INCLUDE_DIR
+        NAMES qrencode.h
+        HINTS ${BREW_HINT}
+        PATHS ${PC_QREncode_INCLUDE_DIRS}
+    )
+endif()
 
 set(QREncode_INCLUDE_DIRS "${QREncode_INCLUDE_DIR}")
 mark_as_advanced(QREncode_INCLUDE_DIR)
 
-# TODO: extract a version number.
-# For now qrencode does not provide an easy way to extract a version number.
-
 include(ExternalLibraryHelper)
 find_component(QREncode qrencode
-	NAMES qrencode
-	HINTS ${BREW_HINT}
-	PATHS ${PC_QREncode_LIBRARY_DIRS}
-	INCLUDE_DIRS ${QREncode_INCLUDE_DIRS}
+    NAMES qrencode
+    HINTS ${BREW_HINT}
+    PATHS ${PC_QREncode_LIBRARY_DIR}
+    INCLUDE_DIRS ${QREncode_INCLUDE_DIR}
 )
 
 include(FindPackageHandleStandardArgs)
@@ -50,3 +55,4 @@ find_package_handle_standard_args(QREncode
 		QREncode_INCLUDE_DIR
 	HANDLE_COMPONENTS
 )
+include_directories(${QREncode_INCLUDE_DIR})
