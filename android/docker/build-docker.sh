a9989244dc131fb890078f7c9c68cd6e8d46c558
TomZ <tom@flowee.org>
2022-10-07 14:12:18 +0200

    Latest Qt

--- android/docker/build-docker.sh
+++ android/docker/build-docker.sh
@@ -14,7 +14,7 @@ if test "$1" != "force"; then
     fi
 fi
 
-QtVersion=v6.3.1
+QtVersion=v6.4.0
 
 cd `dirname $0`
 docker build . --tag flowee/buildenv-android:$QtVersion --build-arg QtVersion=$QtVersion
