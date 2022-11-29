48b47305b53c224549179c6aa9a322f74b71e95b
TomZ <tom@flowee.org>
2022-11-29 10:53:36 +0100

    Downgrade the AURs for SDK.
    Qt hardcodes a gradle version that doesn't like the newest version
    of Android.

--- android/docker/scripts/aurs.sh
+++ android/docker/scripts/aurs.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 # this runs as root.
 
-for i in android-ndk android-sdk-platform-tools android-sdk-cmdline-tools-latest android-platform android-sdk-build-tools
-do
+function makeAur(
     cd ~builduser
-    git clone https://aur.archlinux.org/$i.git
-    chown builduser:builduser -R $i
-    cd "$i"
-    for f in /usr/local/cache/$i*zst; do
-        ln -s "$f" .
+    pkg="$1"
+    git clone https://aur.archlinux.org/$pkg.git
+    cd "$pkg"
+    commit=$2
+    if test -n "$commit"
+    then
+        git checkout $commit
+    fi
+    chown builduser:builduser -R . .git
+    for i in /usr/local/cache/$pkg*zst; do
+        ln -s "$i" .
     done
-    for f in /usr/local/cache/*zip; do
-        ln -s "$f" .
+    for i in /usr/local/cache/*zip; do
+        if test -f $i; then
+            ln -s "$i" .
+        fi
     done
     su builduser -c makepkg
     find . -type f -name '*zst' -exec ln "{}" /usr/local/cache ';'
-done
+)
+
+
+makeAur android-ndk
+# For now we hardcode the 31.0.3 release of the sdk, seeing as Qt hardcodes gradle 7.4.2, which doesn't like a newer sdk.
+makeAur android-sdk-platform-tools 89f6840df092ea2f1fc3d446c41be78cf9b66339
+makeAur android-sdk-build-tools 87aea36e5aef112d7af0c2ae5db154e96ab633c3
+makeAur android-sdk-cmdline-tools-latest
+makeAur android-platform
 
 pacman -U --noconfirm /usr/local/cache/*zst
+
