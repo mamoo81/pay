dad6f5479025b2f0846a5ff7c1546af7d1d4c721
TomZ <tom@flowee.org>
2023-11-30 21:16:52 +0100

    New bugfix version

--- src/main.cpp
+++ src/main.cpp
@@ -90,7 +90,7 @@ int main(int argc, char *argv[])
     QGuiApplication qapp(argc, argv);
     qapp.setOrganizationName("flowee");
     qapp.setApplicationName("pay");
-    qapp.setApplicationVersion("2023.11.0");
+    qapp.setApplicationVersion("2023.11.1");
     qapp.setWindowIcon(QIcon(":/FloweePay.png"));
 
     qmlRegisterType<WalletEnums>("Flowee.org.pay", 1, 0, "Wallet");
