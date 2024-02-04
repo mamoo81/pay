2facea90d8c9a0a47b11b1cd5f736cc014e4e620
TomZ <tom@flowee.org>
2024-02-04 16:39:08 +0100

    Make this compile with older ZXIng libs too

--- src/QRCreator.cpp
+++ src/QRCreator.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Flowee project
- * Copyright (C) 2018-2023 Tom Zander <tom@flowee.org>
+ * Copyright (C) 2018-2024 Tom Zander <tom@flowee.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,6 +23,9 @@
 #include <ZXing/MultiFormatWriter.h>
 #include <ZXing/BitMatrix.h>
 
+#include <codecvt>
+#include <locale>
+
 QRCreator::QRCreator(QRType type)
     : QQuickImageProvider(QQmlImageProviderBase::Image),
     m_type(type)
@@ -33,7 +36,7 @@ QImage QRCreator::requestImage(const QString &id, QSize *size, const QSize &requ
 {
     Q_UNUSED(size);
     Q_UNUSED(requestedSize);
-    std::string data; // assumed utf8 by zxing
+    std::string data; // assumed utf8
     if (m_type == URLEncoded) {
         QUrl url(id); // go via URL to encode spaces and special chars
         data = url.toEncoded();
@@ -42,7 +45,20 @@ QImage QRCreator::requestImage(const QString &id, QSize *size, const QSize &requ
     }
 
     auto writer = ZXing::MultiFormatWriter(ZXing::BarcodeFormat::QRCode).setMargin(16);
-    ZXing::BitMatrix matrix = writer.encode(data, 250, 250);
+    /*
+     * In newer versions of zxing there is a direct std::string version of the encode()
+     * call which is nice to avoid the extra coversion.
+     * For now we leave this extra code here as long as we are still able to compile and
+     * run on Ubuntu 2022.04 (jammy) which doesn't have this new call.
+     *
+     * Ironically, the codecvt below code is deprecated in C++17, so you get warnings now.
+     * Can't win this one, I guess...
+     * But the promise is that it will be part of C++ till the 2026 release,
+     * and I prefer it actually compiling on older zxing.  So maybe lets just
+     * plan to remove the wstring conversion in a year or so (TZ: Feb 2024)
+     */
+    std::wstring wdata = std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(data);
+    ZXing::BitMatrix matrix = writer.encode(wdata, 250, 250);
 
     QImage result = QImage(matrix.height(), matrix.width(), QImage::Format_RGB32);
     constexpr uint black = 0xFF000000;
